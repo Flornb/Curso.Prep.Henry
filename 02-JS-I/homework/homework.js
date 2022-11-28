cb657d0dc7d135c8bc1bb35f3832c0a78f1101dc
Flornb <Florencia@DESKTOP-LT8SQ0N>
2022-11-28 14:11:25 -0300

    completo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -192,28 +192,29 @@ function combinarNombres(nombre, apellido) {
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
-  // Tu código:
-  
+  // Tu código:2-
+  var saludo = "Hola" + " " + nombre + "!";
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return alto*ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado *4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+return (base*altura)/2;
 }
 
 
@@ -221,7 +222,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return 1.20*euro;
 }
 
 
@@ -231,7 +232,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length>1) {
+    return "Dato incorrecto";
+  }
+  if (letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u") {
+    return "Es vocal";
+  } 
+  else {
+    return "Dato incorrecto";
+  }
 }
 
 
